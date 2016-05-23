a1e0a682da6bb4ce499bdbd446f9d537649f11b5
Dean Siroky <dcsiroky@gmail.com>
2016-05-23 16:12:13 -0400

    Added company images to listview.

--- app/src/main/java/turntotech/org/navigationcontroller/fragments/CompanyFragment.java
+++ app/src/main/java/turntotech/org/navigationcontroller/fragments/CompanyFragment.java
@@ -15,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import turntotech.org.navigationcontroller.CustomListAdapter;
 import turntotech.org.navigationcontroller.R;
 
 
@@ -27,8 +28,7 @@ public class CompanyFragment extends ListFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View mCustomView = inflater.inflate(R.layout.custom_actionbar, null);
         TextView title = (TextView)mCustomView.findViewById(R.id.title_text);
@@ -37,13 +37,15 @@ public class CompanyFragment extends ListFragment {
         ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
         actionBar.setCustomView(mCustomView);
         actionBar.setDisplayShowCustomEnabled(true);
-        title.setText("Watch List");
+        title.setText(R.string.watch_list);
 
-        String[] companies = new String[] { "Apple", "Samsung" };
+        String[] companies = new String[] { "Apple", "Microsoft", "Samsung", "Sony" };
+
+        int[] icons = { R.drawable.apple_logo, R.drawable.microsoft_logo, R.drawable.samsung_logo, R.drawable.sony_logo };
 
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, companies);
-        setListAdapter(arrayAdapter);
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, companies);
+        setListAdapter(new CustomListAdapter(getActivity(), companies, icons));
 
         return inflater.inflate(R.layout.fragment_list, container, false);
     }
@@ -53,13 +55,13 @@ public class CompanyFragment extends ListFragment {
         // TODO Auto-generated method stub
         super.onListItemClick(l, v, position, id);
 
-
-
-        TextView title = (TextView)v.findViewById(android.R.id.text1);
+        //TextView title = (TextView)v.findViewById(android.R.id.text1);
+        String s = (String) ((TextView)v.findViewById(R.id.txtStatus)).getText();
 
         Bundle bundle = new Bundle();
         bundle.putInt("CompanyIndex", position);
-        bundle.putString("CompanyTitle", title.getText().toString());
+        //bundle.putString("CompanyTitle", title.getText().toString());
+        bundle.putString("CompanyTitle", s);
 
         productFragment.setArguments(bundle);
 
--- app/src/main/java/turntotech/org/navigationcontroller/fragments/ProductFragment.java
+++ app/src/main/java/turntotech/org/navigationcontroller/fragments/ProductFragment.java
@@ -6,27 +6,26 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import turntotech.org.navigationcontroller.CustomListAdapter;
 import turntotech.org.navigationcontroller.R;
 
 
 public class ProductFragment extends ListFragment {
+
+    ProductFragment productFragment;
 
     public ProductFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
         View mCustomView = inflater.inflate(R.layout.custom_actionbar, null);
@@ -50,20 +49,61 @@ public class ProductFragment extends ListFragment {
 
 
         String[] products = null;
+        int[] icons = null;
 
-        if(companyPosition==0){
-            products = new String[] { "iPhone", "iPAD" };
+        if(companyPosition == 0){
+            products = new String[] { "iPhone", "iPad", "iPod", "iMac", "Mac Book", "Watch" };
+            icons = new int[] {R.drawable.i_phone, R.drawable.i_pad, R.drawable.i_pod,
+                    R.drawable.i_mac, R.drawable.macbook, R.drawable.apple_watch };
         }
-        if(companyPosition==1){
-            products = new String[] { "Galaxy Note", "Galaxy Tab" };
+
+        if(companyPosition == 1){
+            products = new String[] { "Surface", "X-Box", "Windows", "Office", "Smart-Phone" };
+            icons = new int[] {R.drawable.microsoft_surface, R.drawable.x_box, R.drawable.ms_windows,
+                    R.drawable.ms_office, R.drawable.ms_phone };
+        }
+
+        if(companyPosition == 2){
+            products = new String[] { "Galaxy Note", "Galaxy Tab", "Galaxy Gear", "TV", "Home Appliances" };
+            icons = new int[] {R.drawable.galaxy_note, R.drawable.galaxy_tab, R.drawable.galaxy_gear,
+                    R.drawable.samsung_tv_logo, R.drawable.samsung_logo };
+        }
+
+        if(companyPosition == 3){
+            products = new String[] { "Playstation", "Camera", "Audio", "TV", "Mobile Devices" };
+            icons = new int[] {R.drawable.sony_playstation, R.drawable.sony_camera, R.drawable.sony_logo,
+                    R.drawable.sony_logo, R.drawable.sony_mobile };
         }
 
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, products);
-        setListAdapter(arrayAdapter);
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, products);
+        setListAdapter(new CustomListAdapter(getActivity(), products, icons));
 
         return inflater.inflate(R.layout.fragment_list, container, false);
     }
+
+//    @Override
+//    public void onListItemClick(ListView l, View v, int position, long id) {
+//        // TODO Auto-generated method stub
+//        super.onListItemClick(l, v, position, id);
+//
+//        TextView title = (TextView)v.findViewById(android.R.id.text1);
+//
+//        Bundle bundle = new Bundle();
+//        bundle.putInt("CompanyIndex", position);
+//        bundle.putString("CompanyTitle", title.getText().toString());
+//
+//        productFragment.setArguments(bundle);
+//
+//
+//        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//        transaction.addToBackStack(null);
+//        transaction.replace(R.id.fragment_container, productFragment);
+//        transaction.commit();
+//
+//
+//
+//    }
 
 
 
